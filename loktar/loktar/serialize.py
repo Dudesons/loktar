d452fcb48d73b80d254a52f888b40ce3064b90a7
damien <damdam.gold@gmail.com>
2016-04-28 22:35:37 -0700

    ENH: Add a serializer for some type of data like dependency cyle who transfer by http

---
+++ loktar/loktar/serialize.py
@@ -0,0 +1,22 @@
+import base64
+import pickle
+
+
+def serialize(item):
+    """Serialize an item
+    Args:
+        item (object): Python object
+    Returns:
+        str: The item serialized as a string
+    """
+    return base64.b64encode(pickle.dumps(item)).replace("=", "A")
+
+
+def unserialize(item):
+    """Unserialize an item
+    Args:
+        item (str): String to decode
+    Returns:
+        object: Python object
+    """
+    return pickle.loads(base64.b64decode(item))
