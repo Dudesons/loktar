9df422ba30e4527f04e103a4349f035f4ef6ffc2
damien <damdam.gold@gmail.com>
2016-05-10 14:16:35 -0700

    DOC: document the job wrapper db

--- loktar/loktar/db.py
+++ loktar/loktar/db.py
@@ -13,12 +13,32 @@ class Job(object):
         self._pipeline = self._db.pipeline()
 
     def set_job(self, job_id, job_payload):
+        """Save job information in a db
+        Args:
+            job_id (str): the id of the current job
+            job_payload (dict): the payload of the job who is store in the db
+
+        Return:
+             boolean, if the set on the db is ok True or if it failed False
+        """
         return self._db.set(job_id, pickle.dumps(job_payload), ex=self.expire)
 
     def get_job(self, job_id):
+        """Get a specific job payload
+        Args:
+            job_id (str): the id of the current job
+
+        Return:
+             dict, return the job payload
+        """
         return pickle.loads(self._db.get(job_id))
 
     def get_jobs(self):
+        """Get information on all jobs
+
+        Return:
+             list, who represents all payload for current job
+        """
         for job_id in self._db.scan_iter():
             self._pipeline.get(job_id)
 
