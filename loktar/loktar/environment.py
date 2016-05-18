0f16bdcf6bc72d60305c42a34b96a4f3089d9751
damien <damien.goldenberg@tinyclues.com>
2016-05-18 01:30:42 -0700

    ENH: Add some functions around packaging test archive and configuration

--- loktar/loktar/environment.py
+++ loktar/loktar/environment.py
@@ -1,6 +1,18 @@
+import boto.s3 as s3
+import boto.s3.lifecycle as s3_lifecyle
 from fabric.api import env
+from fabric.api import lcd
+from fabric.api import local
+from fabric.exceptions import NetworkError
+import json
 import os
+from uuid import uuid4
 
+from loktar.cmd import exec_command_with_retry
+from loktar.decorators import retry
+from loktar.exceptions import PrepareEnvFail
+from loktar.exceptions import UnknownStorageMethod
+from loktar.log import Log
 
 env.disable_known_hosts = True
 env.warn_only = True
@@ -44,7 +56,7 @@ GITHUB_INFO = {
     "default_master_branch": os.getenv("{0}GITHUB_DEFAULT_MASTER_BRANCH".format(PREFIX_ENV_VAR), "master"),
     "manifest_path": os.getenv("{0}GITHUB_MANIFEST_PATH".format(PREFIX_ENV_VAR), "Manifest")
 }
-GITHUB_TOKEN = os.getenv("{0}GITHUB_TOKEN".format(PREFIX_ENV_VAR), '8b73920d962eb81240b47fc26ba205d94166d3bb')
+GITHUB_TOKEN = os.getenv("{0}GITHUB_TOKEN".format(PREFIX_ENV_VAR), None)
 GITHUB_API_COMMITS = {
     "url": "https://api.github.com/repos/{0}/{1}/git/commits".format(GITHUB_INFO["organization"],
                                                                      GITHUB_INFO["repository"]),
@@ -101,3 +113,147 @@ SLAVE_ENVIRONMENT = {
             .format(ROOT_PATH["container"]),
     "env_file": os.getenv("{0}ENV_FILE".format(PREFIX_ENV_VAR), None)
 }
+
+AWS = {
+    "REGION": os.getenv("{0}AWS_REGION".format(PREFIX_ENV_VAR), None),
+    "BUCKET": os.getenv("{0}AWS_BUCKET".format(PREFIX_ENV_VAR), None)
+}
+
+
+def prepare_test_env(branch, github_organization=None, github_repository=None):
+    """Prepare the test environment
+
+    Args:
+        branch (str): Name of the branch the repository should be checkout to.
+        github_organization (str): this is the github organization for get back the repository, default value None.
+                                   Also can be set by environment variable LOKTAR_GITHUB_INFO_ORGANIZATION
+        github_repository (str): this is the target repository to download, default value None
+                                 Also can be set by environment variable LOKTAR_GITHUB_INFO_REPOSITORY
+
+    Raises:
+        PrepareEnvFail: Failed to prepare the environment.
+    """
+    logger = Log()
+    unique_name_dir = str(uuid4())
+    unique_path_dir = "/tmp/{0}".format(unique_name_dir)
+
+    logger.info("Preparing the test environment")
+
+    os.mkdir(unique_path_dir)
+    try:
+        if not exec_command_with_retry("git clone -b {0} --single-branch git@github.com:{1}/{2}.git {3}"
+                                       .format(branch,
+                                               GITHUB_INFO["organization"]
+                                               if github_organization is None else
+                                               github_organization,
+                                               GITHUB_INFO["repository"]
+                                               if github_repository is None else
+                                               github_repository,
+                                               unique_path_dir),
+                                       0,
+                                       MAX_RETRY_GITHUB):
+            raise PrepareEnvFail
+
+        with lcd(unique_path_dir):
+            if not exec_command_with_retry("git fetch origin master", 0, MAX_RETRY_GITHUB):
+                raise PrepareEnvFail
+
+            if branch != "master":
+                if not exec_command_with_retry("git config --global user.email 'you@example.com'", 0, MAX_RETRY_GITHUB):
+                    raise PrepareEnvFail
+
+                if not exec_command_with_retry("git config --global user.name 'Your Name'", 0, MAX_RETRY_GITHUB):
+                    raise PrepareEnvFail
+
+                if not exec_command_with_retry("git merge --no-ff --no-edit FETCH_HEAD", 0, MAX_RETRY_GITHUB):
+                    raise PrepareEnvFail
+
+                local("rm -rf {0}/.git".format(unique_path_dir))
+
+        with lcd("/tmp"):
+            if not exec_command_with_retry("tar -czf {0}.tar {0}".format(unique_name_dir), 0, MAX_RETRY_GITHUB):
+                raise PrepareEnvFail
+
+        logger.info("The test env is ready!")
+
+    except NetworkError as exc:
+        logger.error(exc)
+        raise
+    except PrepareEnvFail:
+        local("rm -rf {0}*".format(unique_path_dir))
+        raise
+
+    return unique_path_dir
+
+
+def get_config(package_name, test_env_path, full=False):
+    """Retrieve the test configuration
+
+    Args:
+        package_name: Name of the package for which to apply ``full=False``
+        test_env_path: the location of the cloned test environment.
+        full (boolean): See ``Returns`` section
+
+    Returns:
+        if ``full`` True, return the complete config.json file.
+        Otherwise, return a dict with keys: ``['pkg_name', 'pkg_type', 'test_type']``
+    """
+    pkg_config = {}
+    logger = Log()
+
+    with lcd('{0}'.format(test_env_path)):
+        config = json.loads(local('cat config.json', capture=True))
+
+    logger.info('Parsing configuration')
+    if not full:
+        for package in config['packages']:
+                if package['pkg_name'] == package_name:
+                    pkg_config = package
+                    break
+    else:
+        pkg_config = config
+
+    logger.info('Conf is ok and has keys {0}'.format(pkg_config.keys()))
+    return pkg_config
+
+
+@retry
+def store_test_env_on_s3(target, aws_region, aws_bucket):
+    """Store an archive on aws s3
+
+    Args:
+        target (str): this is the archive target
+        aws_region (str, None): this is the aws region to connect for the bucket,
+            aws_region can be set by a variable environment AWS_REGION, this variable it takes in first if it set
+            can be set at None
+        aws_bucket (str, None): this is the bucket name where the archive will be stored
+            aws_bucket can be set by a variable environment AWS_BUCKET, this variable it takes in first if it set
+
+
+    Returns
+        A string who debribes the archive (eg: s3:@:foobar foobar here is the archive name)
+    """
+    key_name = target.split("/")[-1]
+    connexion = s3.connect_to_region(AWS["REGION"] if AWS["REGION"] is not None else aws_region)
+    bucket = connexion.get_bucket(AWS["BUCKET"] if AWS["BUCKET"] is not None else aws_bucket)
+    key = bucket.new_key(key_name)
+    key.set_contents_from_filename("{0}.tar".format(target))
+
+    return "s3:/{0}/{1}".format(AWS["BUCKET"], key_name)
+
+
+def store_test_env(store_type, target, **kwargs):
+    """Store the archive for launching test
+
+    Args:
+        store_type (str): define the storage type
+        target (str): this is the archive target
+        kwargs (dict): have some information around the storing method (eg: s3 need the bucket and region information)
+
+    Returns
+        A string who begin with the method for get back the archive and the archive name (eg: s3:@:foobar)
+    """
+    if store_type == "s3":
+        return store_test_env_on_s3(target, kwargs.get("aws_region", None), kwargs.get("aws_bucket", None))
+    else:
+        raise UnknownStorageMethod
