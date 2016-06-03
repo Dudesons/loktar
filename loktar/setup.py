f4e43fae33509e994d22a3812f79d0ff7867760c
damien <damien.goldenberg@tinyclues.com>
2016-06-03 03:32:35 -0700

    BLD: Upgrade version

--- loktar/setup.py
+++ loktar/setup.py
@@ -15,7 +15,7 @@ with open('README.md') as readme_file:
 requirements = [str(i.req) for i in parse_requirements("requirements.txt", session=False)]
 test_requirements = [str(i.req) for i in parse_requirements("test_requirements.txt", session=False)]
 
-VERSION = '1'
+VERSION = '3'
 
 setup(
     name='loktar',
