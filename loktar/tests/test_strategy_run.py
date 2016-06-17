8c465d85444eedee67a5362a584f5648e20ac59b
damien <damien.goldenberg@tinyclues.com>
2016-06-17 00:28:45 -0700

    CLN: the method is  static it doesn't need a self

--- loktar/tests/test_strategy_run.py
+++ loktar/tests/test_strategy_run.py
@@ -13,7 +13,7 @@ def test_strategy_run(mocker, package, run_type, remote):
             pass
 
         @staticmethod
-        def run(self):
+        def run():
             print("This is an awesome plugin")
 
     mocker.patch("loktar.strategy_run.find_plugin", return_value=Fake)
