c58f5c3a52145d90aed685cb8d7e17e11dca0d14
damien <damien.goldenberg@tinyclues.com>
2016-06-20 06:27:14 -0700

    TST: Reorganize the basic test for strategy running plugin

--- loktar/tests/test_strategy_run.py
+++ loktar/tests/test_strategy_run.py
@@ -9,12 +9,13 @@ from loktar.strategy_run import strategy_runner
 @pytest.mark.parametrize("remote", [True, False])
 def test_strategy_run(mocker, package, run_type, remote):
     class Fake(object):
-        def __init__(self, *args, **kwargs):
-            pass
-
         @staticmethod
         def run(*args, **kwargs):
             print("This is an awesome plugin")
+            Fake(args[0], args[1])
+
+        def __init__(self, *args, **kwargs):
+            print("Inside of this awesome plugin")
 
     mocker.patch("loktar.strategy_run.find_plugin", return_value=Fake)
     strategy_runner(package, run_type, remote=remote)
