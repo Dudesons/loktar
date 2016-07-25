dcabc5e39e31833c5cfd2f4aa7415ff7d47c4838
dgo <damien.goldenberg@tinyclues.com>
2016-07-25 19:31:03 +0200

    CLN: Rename some functions

--- loktar/src/tests/test_store.py
+++ loktar/src/tests/test_store.py
@@ -1,34 +1,34 @@
 import pytest
 
 from loktar.exceptions import UnknownStorageMethod
-from loktar.store import _get_back_test_env_from_s3
+from loktar.store import _get_back_artifact_from_s3
 from loktar.store import get_back_test_env
-from loktar.store import store_test_env
+from loktar.store import store_artifact
 
-from loktar.store import _store_test_env_on_s3
+from loktar.store import _store_artifact_on_s3
 
 
-def test_store_test_env_on_s3(mocker):
+def test_store_artifact_on_s3(mocker):
     mocker.patch("loktar.store.s3")
-    assert "s3:@:toto" == _store_test_env_on_s3("/tmp/toto", None, None)
+    assert "s3:@:toto" == _store_artifact_on_s3("/tmp/toto", None, None)
 
 
 @pytest.mark.parametrize("store_type", ["s3"])
-def test_store_test_env(mocker, store_type):
+def test_store_artifact(mocker, store_type):
     mocker.patch("loktar.store.s3")
-    assert "{0}:@:toto".format(store_type) == store_test_env(store_type, "/tmp/toto")
+    assert "{0}:@:toto".format(store_type) == store_artifact(store_type, "/tmp/toto")
 
 
 @pytest.mark.parametrize("store_type", ["foobar"])
-def test_store_test_env_fail_on_unknown_storage_method(mocker, store_type):
+def test_store_artifact_fail_on_unknown_storage_method(mocker, store_type):
     mocker.patch("loktar.store.s3")
     with pytest.raises(UnknownStorageMethod):
-        store_test_env(store_type, "/tmp/toto")
+        store_artifact(store_type, "/tmp/toto")
 
 
 def test_get_back_test_env_on_s3(mocker):
     mocker.patch("loktar.store.s3")
-    assert "/tmp/toto" == _get_back_test_env_from_s3("toto", None, None)
+    assert "/tmp/toto" == _get_back_artifact_from_s3("toto", None, None)
 
 
 @pytest.mark.parametrize("target", ["s3:@:toto"])
