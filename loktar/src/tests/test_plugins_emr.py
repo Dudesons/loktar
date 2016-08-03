81bd74316c826706cf0e95da48cb823fbe1da59e
dgo <damien.goldenberg@tinyclues.com>
2016-08-03 15:07:07 +0200

    TST: Enhance the coverage test

--- loktar/src/tests/test_plugins_emr.py
+++ loktar/src/tests/test_plugins_emr.py
@@ -1,5 +1,6 @@
 import pytest
 
+from loktar.exceptions import CIBuildPackageFail
 from loktar.exceptions import CITestFail
 from loktar.plugins.artifact.emr import EMR
 from loktar.plugins.artifact.emr import run
@@ -22,7 +23,7 @@ def ci_payload():
 
 @pytest.mark.parametrize("remote", [True, False])
 @pytest.mark.parametrize("mode", ["master", "foobranch"])
-def test_plugins_make(mocker, remote, ci_payload, mode):
+def test_plugins_emr(mocker, remote, ci_payload, mode):
     mocker.patch("loktar.plugin.exe")
 
     ci_payload["mode"] = mode
@@ -32,7 +33,7 @@ def test_plugins_make(mocker, remote, ci_payload, mode):
 
 @pytest.mark.parametrize("remote", [True, False])
 @pytest.mark.parametrize("mode", ["master", "foobranch"])
-def test_plugins_make_fail_on_command(mocker, remote, ci_payload, mode):
+def test_plugins_emr_fail_on_command(mocker, remote, ci_payload, mode):
     mocker.patch("loktar.plugin.exe", return_value=False)
 
     ci_payload["mode"] = mode
@@ -41,7 +42,20 @@ def test_plugins_make_fail_on_command(mocker, remote, ci_payload, mode):
         run(ci_payload, remote)
 
 
-def test_plugins_make_fail_on_call():
+def test_plugins_emr_fail_on_call():
     with pytest.raises(TypeError):
         output = run()
         assert output == EMR.__init__.__doc__
+
+
+@pytest.mark.parametrize("remote", [True, False])
+def test_plugins_emr_fail_on_input_type(mocker, remote, ci_payload):
+    mocker.patch("loktar.plugin.exe")
+
+    input_type = "the new and awesome input type"
+    ci_payload["build_info"]["input_type"] = input_type
+
+    with pytest.raises(CIBuildPackageFail) as excinfo:
+        run(ci_payload, remote)
+        assert str(excinfo) == "the input type : '{}' is not managed, create a pr for integrate this input type"\
+                               .format(input_type)
