114a876c6dc03182399cbc82f9f456352694e1fd
dgo <damien.goldenberg@tinyclues.com>
2017-01-12 16:53:52 +0100

    ENH/CLN: Update plugins

--- loktar/src/loktar/plugins/dependency/python_requirements.py
+++ loktar/src/loktar/plugins/dependency/python_requirements.py
@@ -13,15 +13,20 @@ def python_deps(basepath):
     """
     requirements = []
 
-    requirements_file = os.path.join(basepath, 'requirements.txt')
-    test_requirements_file = os.path.join(basepath, 'test_requirements.txt')
-    app_requirements_file = os.path.join(basepath, 'app', 'requirements.txt')
-    app_test_requirements_file = os.path.join(basepath, 'app', 'test_requirements.txt')
+    requirements_file = os.path.join(basepath, "requirements.txt")
+    test_requirements_file = os.path.join(basepath, "test_requirements.txt")
+    app_requirements_file = os.path.join(basepath, "app", "requirements.txt")
+    app_test_requirements_file = os.path.join(basepath, "app", "test_requirements.txt")
+    src_requirements_file = os.path.join(basepath, "src", "requirements.txt")
+    src_test_requirements_file = os.path.join(basepath, "src", "test_requirements.txt")
 
-    for req_file in (requirements_file, test_requirements_file, app_requirements_file, app_test_requirements_file):
+    for req_file in (requirements_file, test_requirements_file, app_requirements_file,
+                     app_test_requirements_file, src_requirements_file, src_test_requirements_file):
         if os.path.exists(req_file):
-            with open(req_file, 'r') as f:
+            with open(req_file, "r") as f:
                 requirements.extend(f.readlines())
 
-    requirements_names = set(filter(None, map(lambda x: re.split('==|>=|>|<=|<', x)[0], requirements)))
+    requirements_names = set(filter(None, map(lambda x: re.split("==|>=|>|<=|<", x)[0], requirements)))
     return requirements_names
+
+Plugin = python_deps
