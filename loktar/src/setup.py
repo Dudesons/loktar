673f97cb941550cb89a8fa9468cfe5d0a2b485df
dgo <damien.goldenberg@tinyclues.com>
2017-02-06 15:47:58 +0100

    BLD: Bump to version 42

--- loktar/src/setup.py
+++ loktar/src/setup.py
@@ -15,7 +15,7 @@ with open('README.md') as readme_file:
 requirements = [str(i.req) for i in parse_requirements("requirements.txt", session=False)]
 test_requirements = [str(i.req) for i in parse_requirements("test_requirements.txt", session=False)]
 
-VERSION = '31'
+VERSION = '42'
 
 setup(
     name='loktar',
