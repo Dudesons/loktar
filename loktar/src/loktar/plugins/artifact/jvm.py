4d7167a283ec1a260d961ac9dd2bc0f83043968d
Quentin Suire <quentin.suire@tinyclues.com>
2017-02-27 17:03:12 +0100

    add maven to jvm plugin

--- loktar/src/loktar/plugins/artifact/jvm.py
+++ loktar/src/loktar/plugins/artifact/jvm.py
@@ -25,12 +25,13 @@ class JVM(object):
 
             """
             if artifact_info["build_info"]["build_type"] == "sbt":
-                self.__result = _SBT(artifact_info, remote).run()
-            # elif artifact_info["build_info"]["build_type"] == "maven":
-            #     self.__result = _MVN(artifact_info, remote).run()
+                plugin = _SBT(artifact_info, remote)
+            elif artifact_info["build_info"]["build_type"] == "maven":
+                plugin = _Maven(artifact_info, remote)
             else:
                 raise CIBuildPackageFail("the build type: '{}' isn't managed, create a pr for integrate this build type"
                                          .format(artifact_info["build_info"]["build_type"]))
+            self.__result = plugin.run()
 
         def get_result(self):
             return self.__result
@@ -76,6 +77,41 @@ class _SBT(SimplePlugin):
                         raise CIBuildPackageFail("Can't find build.sbt")
                     else:
                         self.path = "{}/{}".format(self.path, output[0])
+
+    def run(self):
+        self._base_run()
+        return {
+        }
+
+
+class _Maven(SimplePlugin):
+    def __init__(self, artifact_info, remote):
+        """Plugin for building jvm artifact with maven
+
+            Args:
+                artifact_info (dict): Contains information about the package to execute inside the plugin
+                remote (bool): Define if the plugin will be execute in remote or not
+
+            Raises:
+                CIBuildPackageFail: when one of the steps for packaging or uploading the package failed
+        """
+        if artifact_info["mode"] == "master":
+            sbtcmd = "{} mvn -DskipTests=true --batch-mode release:prepare release:perform".format(
+                artifact_info["build_info"].get("prefix_command", "")
+            )
+        else:
+            sbtcmd = "{} mvn deploy".format(
+                artifact_info["build_info"].get("prefix_command", "")
+            )
+
+        SimplePlugin.__init__(self, artifact_info,
+                              {
+                                  "command": {
+                                      "run": sbtcmd.strip(),
+                                      "clean": artifact_info.get("clean_method", "make clean")
+                                  }
+                              },
+                              remote=remote)
 
     def run(self):
         self._base_run()
