b50a32903d85c49673eb0e5b9639c3b5b17b74eb
dgo <damien.goldenberg@tinyclues.com>
2017-03-03 15:46:09 +0100

    ENH: Force pull image to prevent strange error

--- loktar/src/loktar/container.py
+++ loktar/src/loktar/container.py
@@ -2,10 +2,11 @@ from docker import DockerClient
 import time
 
 
-def start_container(image, environment, ports_settings, docker_endpoint="127.0.0.1:2375", network_mode="bridge"):
+def start_container2(image, environment, ports_settings, docker_endpoint="127.0.0.1:2375", network_mode="bridge"):
     container_infos = dict()
     client = DockerClient(base_url=docker_endpoint)
     client.login(username="")
+    client.images.pull(image)
     container = client.containers.run(image,
                                       detach=True,
                                       environment=environment,
@@ -13,13 +14,11 @@ def start_container(image, environment, ports_settings, docker_endpoint="127.0.0
                                       network_mode=network_mode)
     container.start()
     time.sleep(2)
-
     container = client.containers.get(container.id)
     container_infos["host"] = container.attrs["Node"]["IP"]
     container_infos["host_port"] = int(sorted(container.attrs["NetworkSettings"]["Ports"].values(),
                                               reverse=True)[0][0]["HostPort"])
     container_infos["id"] = container.id
-
     return container_infos
 
 
