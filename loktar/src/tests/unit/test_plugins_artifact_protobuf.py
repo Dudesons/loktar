5571f97dcce25e29ba7db2e6ad46008f748ff423
damien-tinyclues <damien.goldenberg@tinyclues.com>
2017-06-05 15:23:45 +0200

    Support protobuff artifact (#20)
    * ENH: Create the protobuff plugin
    
    * TST: Add unit test for protobuff plugin
    
    * CLN: Fix typo
    
    * BUG: Fix ssh communication
    
    * ENH: Give the full job config in the build_info part
    
    * ENH: Can return an empty dict if there is no version

---
+++ loktar/src/tests/unit/test_plugins_artifact_protobuf.py
@@ -0,0 +1,60 @@
+from copy import deepcopy
+
+from flexmock import flexmock
+import pytest
+
+from loktar.plugins.artifact import protobuf
+
+
+@pytest.mark.parametrize("artifact_info", [{
+      "artifact_type": "protobuf",
+      "artifact_name": "events",
+      "test_type": "no-test",
+      "type": "library",
+      "build_info": {
+        "sub_artifact_types": [
+          {"type": "jvm", "remote": True, "build_type": "sbt"},
+          {"type": "whl"}
+        ]
+      },
+      "dependencies_type": [
+        "dockerfile",
+        "python_requirements"
+      ],
+      "notification": {
+        "slack_channels": [
+          "ci-platform"
+        ]
+      }
+    }])
+@pytest.mark.parametrize("remote", [True, False])
+def test_plugins_artifact_protobuf(artifact_info, remote):
+
+    jvm_protobuf_config = deepcopy(artifact_info)
+    jvm_protobuf_config["artifact_type"] = "jvm"
+    jvm_protobuf_config["build_info"].update(artifact_info["build_info"]["sub_artifact_types"][0])
+    whl_protobuf_config = deepcopy(artifact_info)
+    whl_protobuf_config["artifact_type"] = "whl"
+    whl_protobuf_config["build_info"].update(artifact_info["build_info"]["sub_artifact_types"][1])
+
+    flexmock(protobuf) \
+        .should_receive("strategy_runner") \
+        .with_args(jvm_protobuf_config,
+                   "artifact",
+                   remote=True) \
+        .and_return(None)
+
+    flexmock(protobuf) \
+        .should_receive("strategy_runner") \
+        .with_args(whl_protobuf_config,
+                   "artifact",
+                   remote=False) \
+        .and_return({"version": 42})
+
+    plugin = protobuf.Protobuf(artifact_info, remote)
+    plugin.launch_sub_project()
+    assert type(plugin.share_memory) is dict
+    assert "latest_versions" in plugin.share_memory
+    assert "{}-{}-42".format(artifact_info["artifact_name"],
+                             whl_protobuf_config["artifact_type"])\
+           in plugin.share_memory["latest_versions"]
